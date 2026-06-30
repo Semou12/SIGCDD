eec539b2e7b2e505ac1ab8797ef08d26ed459e08
DEVSUDPAY <devsudpay@sud-pay.com>
2026-06-30 20:59:10 +0000

    ajoutpatch  mfa

--- sigcdd/users/apps.py
+++ sigcdd/users/apps.py
@@ -5,3 +5,12 @@ class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
     verbose_name = _("Users")
+
+    def ready(self):
+        # Applique le monkey-patch de allauth.mfa.utils.is_mfa_enabled.
+        # Sans cet appel, allauth utilise sa version d'origine qui se base
+        # uniquement sur la présence d'un Authenticator en base, et ignore
+        # totalement FORCE_2FA_ENABLED / mfa_exempt / ROLES_WITHOUT_2FA.
+        # Voir users/adapters.py::patch_is_mfa_enabled() pour le détail.
+        from users.adapters import patch_is_mfa_enabled
+        patch_is_mfa_enabled()
